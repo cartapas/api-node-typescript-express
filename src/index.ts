9e6fb07ce18a8177abe582eb42195a712ba6d65d
Carlos Tapasco <cartapas@gmail.com>
2025-04-19 23:59:54 -0500

    feat: configuring linter

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import diaryRouter from './routes/diaries';
+import diaryRouter from './routes/diaries'
 
 const app = express()
 app.use(express.json()) // Middleware que transforma la req.body a un JSON.
@@ -7,12 +7,12 @@ app.use(express.json()) // Middleware que transforma la req.body a un JSON.
 const PORT = 3000
 
 app.get('/ping', (_req, res) => {
-    console.log('Someone pinged here!!')
-    res.send('pong')
+  console.log('Someone pinged here!!')
+  res.send('pong')
 })
 
-app.use('/api/diaries', diaryRouter);
+app.use('/api/diaries', diaryRouter)
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
